3ddfc9393689da2b710b8dd6cbb8f5707c6dbe6b
huahanqi <huahanqi@gmail.com>
2023-09-06 17:53:24 -0400

    update readme

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use("/api/v1/tasks", authenticateUser, tasksRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 const start = async () => {
   try {
